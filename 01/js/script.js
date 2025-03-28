6b8b94091552ef62f2d31d94b5b46954618ebe6b
AlvaroCalde <alvaro.calper@educa.jcyl.es>
2025-03-28 11:54:41 +0100

    Ejercicio 2

--- 01/js/script.js
+++ 01/js/script.js
@@ -11,8 +11,14 @@ parrafo[0].outerHTML= "<div>Parrafo 1 modificado</div>"
 
 cuerpo.style.backgroundColor="#f4f4f4"
 cuerpo.style.textAlign="center"
+
 titulo.style.color="#333"
 titulo.style.marginBottom="15px"
 titulo.style.textTransform="uppercase"
+titulo.style.fontWeight="bold"
+
 parrafo.style.width="18px"
 parrafo.style.padding="10px"
+parrafo[0].style.color="#e74c3c"
+
+parrafo[1].style.color="#2ecc71"
